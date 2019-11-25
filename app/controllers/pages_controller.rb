9b6584f6186ef883ed2001f638385ff35a3c4425
Josua Black Rocket <josua@blackrocket.co>
2019-11-25 15:20:52 -0500

    add pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def root
+  end
+
+  def proyecto
+  end
+
+  def caracteristicas
+  end
+
+  def documentacion
+  end
+
+  def soporte
+  end
+
+  def registrarse
+  end
+
+  def inicio_Sesion
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/root'
+  get 'pages/proyecto'
+  get 'pages/caracteristicas'
+  get 'pages/documentacion'
+  get 'pages/soporte'
+  get 'pages/registrarse'
+  get 'pages/inicio_Sesion'
   resources :experiment_tests
   resources :lab_leds
   devise_for :users
